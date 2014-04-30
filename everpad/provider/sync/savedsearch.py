cdd2f1ea9026211d0d619f521b4d835c6e422d66
Mike Greene <mikeos2@gmail.com>
2014-04-30 13:49:48 -0400

    Update savedsearch.py

--- everpad/provider/sync/savedsearch.py
+++ everpad/provider/sync/savedsearch.py
@@ -7,3 +7,86 @@ from ... import const
 from ..exceptions import TTypeValidationFailed
 from .. import models
 from .base import BaseSync, SyncStatus
+
+
+class PullSearch(BaseSync):
+    """Pull Search from server"""
+
+    # Args:
+    #    self.auth_token, self.session,
+    #    self.note_store, self.user_store
+    #
+    def __init__(self, *args, **kwargs):
+        super(PullLBN, self).__init__(*args, **kwargs)
+        self._exists = []
+
+    def pull(self, chunk_start_after, chunk_end):
+        """Pull tags from server"""
+
+        # okay, so _get_all_lbn uses a generator to yield each record
+        for search_meta_ttype in self._get_all_lbn(chunk_start_after, chunk_end):
+
+            # EEE Rate limit from _get_all_notes then break
+            if SyncStatus.rate_limit:
+                break
+            
+            self.app.log(
+                'Pulling search "%s" from remote server.' % search_meta_ttype.name) 
+
+        # @@@@ This file is just a stub
+
+
+    # ************ Get All Linked Notebooks **************
+    #
+    #  Uses getLinkedNotebookSyncChunk to pull LNB data
+    #  from the server and yield each note for processing.
+    #  chunk_start_after will be zero for a full sync and will
+    #  be the local store high USN for increment sync
+    #
+    def _get_all_search(self, chunk_start_after, chunk_end):
+        """Iterate all notes"""
+
+        while True:
+            try:
+            try:
+                sync_chunk = self.note_store.getFilteredSyncChunk(
+                    self.auth_token,
+                    chunk_start_after,
+                    chunk_end,
+                    SyncChunkFilter(
+                        includeSearches=True,
+                    )
+                ) 
+
+            # EEE if a rate limit happens 
+            except EDAMSystemException, e:
+                if e.errorCode == EDAMErrorCode.RATE_LIMIT_REACHED:
+                    self.app.log(
+                        "Rate limit in _get_all_tags: %d minutes" % 
+                            (e.rateLimitDuration/60)
+                    )
+                    SyncStatus.rate_limit = e.rateLimitDuration
+                    break
+            
+            # https://www.jeffknupp.com/blog/2013/04/07/
+            #       improve-your-python-yield-and-generators-explained/
+            # https://wiki.python.org/moin/Generators
+            # Each SyncChunk.tags is yielded (yield note) for 
+            # create or update 
+            for srv_search in sync_chunk.searches:
+                # no notes in this chunk                
+                if not srv_search.guid:
+                    break
+                yield srv_search
+
+            # Here chunkHighUSN is the highest USN returned by the current
+            # getFilteredSyncChunk call.  If chunkHighUSN == chunk_end then
+            # we have received all Note structures on the server so break.
+            # If chunkHighUSN != chunk_end then there is more to get so 
+            # chunk_start_after set to chunkHighUSN which will retrieve 
+            # starting at chunkHighUSN+1 to chunk_end when calling 
+            # getFilteredSyncChunk again - got it?
+            if sync_chunk.chunkHighUSN == sync_chunk.updateCount:
+                break
+            else:
+                chunk_start_after = sync_chunk.chunkHighUSN
