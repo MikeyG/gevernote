36ff0007b0451ea35acfd69fe7b86a69571b6d6e
mgreene <mikeos2@gmail.com>
2014-05-17 10:37:06 -0400

    PyCharm changes - fixing misc

--- everpad/provider/daemon.py
+++ everpad/provider/daemon.py
@@ -1,14 +1,17 @@
-from .service import ProviderService
-from .sync.agent import SyncThread
-from .tools import set_auth_token, get_auth_token, auth_geverpad_token, get_db_session
-from ..specific import AppClass
-from ..tools import print_version
-from . import models
+from everpad.provider.service import ProviderService
+from everpad.provider.sync.agent import SyncThread
+from everpad.provider.tools import set_auth_token, get_auth_token, auth_geverpad_token, get_db_session
+from everpad.specific import AppClass
+from everpad.tools import print_version
+import everpad.provider.models
+
 from PySide.QtCore import Slot, QSettings
+
+# do I need full dbus? MKG
 import dbus
+import dbus.service
 from dbus.mainloop.glib import DBusGMainLoop
-import sqlalchemy
-# import dbus.mainloop.glib
+
 import signal
 import fcntl
 import os
@@ -17,17 +20,18 @@ import argparse
 import sys
 import logging
 
-# Main everpad-provider file - started everpad-provider call to main( )
+# daemon.py Main everpad-provider file - started everpad-provider call to main( )
+
 
 class ProviderApp(AppClass):
 
     def __init__(self, verbose, *args, **kwargs):
-        
+
         # non-kde:
         # from PySide.QtCore import QCoreApplication    
         # AppClass = QCoreApplication
         AppClass.__init__(self, *args, **kwargs)
-        
+
         # ref:  http://qt-project.org/doc/qt-4.8/qsettings.html
         #
         # For example, if your product is called Star Runner and your company 
@@ -35,7 +39,7 @@ class ProviderApp(AppClass):
         #     QSettings settings("MySoft", "Star Runner");
         #  Backwards?
         self.settings = QSettings('everpad', 'everpad-provider')
-        
+
         # debug output - true if verbose passed
         self.verbose = verbose
 
@@ -44,7 +48,6 @@ class ProviderApp(AppClass):
         session_bus = dbus.SessionBus()
         self.bus = dbus.service.BusName("com.everpad.Provider", session_bus)
         self.service = ProviderService(session_bus, '/EverpadProvider')
-
 
         # subclass PySide.QtCore.QThread  - agent.py
         # setup Sync thread
@@ -59,19 +62,19 @@ class ProviderApp(AppClass):
         self.sync_thread.data_changed.connect(
             Slot()(self.service.data_changed),
         )
-                
-        # Start Sync Thread if provider is authenticated        
+
+        # Start Sync Thread if provider is authenticated
         if get_auth_token():
             print("Auth token")
             self.sync_thread.start()
-        else:        
+        else:
             print("No auth token")
-        
-       # ************************************************************
-       #    Authentication and Termination Signals Setup
-       # ************************************************************
-       
-       # provider_authenticate @Slot
+
+            # ************************************************************
+            #    Authentication and Termination Signals Setup
+            # ************************************************************
+
+            # provider_authenticate @Slot
         self.service.qobject.authenticate_signal.connect(
             self.provider_authenticate,
         )
@@ -84,51 +87,51 @@ class ProviderApp(AppClass):
             self.on_remove_authenticated,
         )
         self.service.qobject.terminate.connect(self.terminate)
-        
+
         # ************************************************************
         #                   Configure logger
         # ************************************************************
         # https://docs.python.org/2/library/logging.html
         # good ref: 
         # http://victorlin.me/posts/2012/08/26/good-logging-practice-in-python
-        
+
         logging.basicConfig(level=logging.INFO)
         self.logger = logging.getLogger('everpad-provider')
-        
+
         print("pre logging.getLogger")
-    
+
         # self.logger.setLevel(logging.DEBUG)
-        print("pre logging.getLogger1")        
+        print("pre logging.getLogger1")
         fh = logging.FileHandler(
             os.path.expanduser('~/.everpad/logs/everpad-provider.log'))
         fh.setLevel(logging.DEBUG)
-        print("pre logging.getLogger2")        
-        
+        print("pre logging.getLogger2")
+
         formatter = logging.Formatter(
             '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         fh.setFormatter(formatter)
-        
+
         self.logger.addHandler(fh)
-        
+
         # self.logger.info('Provider started.')
-        
+
         #log('Provider started.')
 
         #log("sqlalchemy %s" % sqlalchemy.__version__)
-        
 
     # ************************************************************
     #          Authentication and Termination 
     # ************************************************************
-       
+
     # add auth MKG
     @Slot(str)
     def provider_authenticate(self):
         # auth_geverpad_token everpad/provider/tools.py 
-        oauth_result = auth_geverpad_token()
-        
+        # oauth_result =
+        auth_geverpad_token()
+
     # get_auth_token --- see tools.py
-        
+
     @Slot(str)
     def on_authenticated(self, token):
         # set_auth_token everpad/provider/tools.py 
@@ -138,7 +141,7 @@ class ProviderApp(AppClass):
     @Slot()
     def on_remove_authenticated(self):
         print("Remove Auth")
-        
+
         self.sync_thread.timer.stop()
         self.sync_thread.quit()
         self.sync_thread.update_count = 0
@@ -146,17 +149,17 @@ class ProviderApp(AppClass):
         # get_db_sesson everpad/provider/tools.py
         set_auth_token('')
         session = get_db_session()
-        
-        session.query(models.Note).delete(
+
+        session.query(everpad.provider.models.Note).delete(
             synchronize_session='fetch',
         )
-        session.query(models.Resource).delete(
+        session.query(everpad.provider.models.Resource).delete(
             synchronize_session='fetch',
         )
-        session.query(models.Notebook).delete(
+        session.query(everpad.provider.models.Notebook).delete(
             synchronize_session='fetch',
         )
-        session.query(models.Tag).delete(
+        session.query(everpad.provider.models.Tag).delete(
             synchronize_session='fetch',
         )
         session.commit()
@@ -177,7 +180,6 @@ class ProviderApp(AppClass):
 
 # creates everpad directories - called from main( )
 def _create_dirs(dirs):
-
     """Create everpad dirs"""
     for path in dirs:
         try:
@@ -185,17 +187,17 @@ def _create_dirs(dirs):
         except OSError:
             continue
 
+
 # kicks things off starting everpad-provider
 def main():
-    
     # ctrl-c terminates the process gracefully
     # http://docs.python.org/dev/library/signal.html
     # http://pymotw.com/2/signal/
     signal.signal(signal.SIGINT, signal.SIG_DFL)
-    
+
     # create everpad directories - _create_dirs local
     _create_dirs(['~/.everpad/', '~/.everpad/data/', '~/.everpad/logs/'])
-    
+
     # parse args using funky python built-in stuff
     # {none}, verbose, or version
     # ref: http://docs.python.org/2/howto/argparse.html
@@ -203,37 +205,39 @@ def main():
     parser.add_argument('--verbose', action='store_true', help='verbose output')
     parser.add_argument('--version', '-v', action='store_true', help='show version')
     args = parser.parse_args(sys.argv[1:])
-    
+
     # print version (tools.py) and exit. print_version executes sys.exit(0)
     # after printing
     if args.version:
         print_version()
-    
+
     # lockfile using usr name getpass.getuser()
     # start main loop or error out
-    fp = open('/tmp/everpad-provider-%s.lock' % getpass.getuser(), 'w')
+    fp = open('/tmp/gverpad-provider-%s.lock' % getpass.getuser(), 'w')
+
     try:
+
         fcntl.lockf(fp, fcntl.LOCK_EX | fcntl.LOCK_NB)
+
         # ref: http://dbus.freedesktop.org/doc/dbus-python/api/
         # set_as_default is given and is true, set the new main 
         # loop as the default for all new Connection or Bus instances
         # allows this script to receive DBus calls
         dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
-        
+
         # SlavikZ - Fix occasional everpad-provider segfaults 050814
         # http://dbus.freedesktop.org/doc/dbus-python/api/dbus.mainloop.glib-module.html
         # Initialize threads in dbus-glib, if this has not already been done.
         dbus.mainloop.glib.threads_init()
-        
+
         # http://stackoverflow.com/questions/22390064/use-dbus-to-just-send-a-message-in-python
-        
         app = ProviderApp(args.verbose, sys.argv)
         app.exec_()
-    
+
     except IOError:
         print("geverpad-provider already running")
     except Exception as e:
-        app.logger.debug(e)
+        print(e)
 
 # allows running daemon.py directly
 if __name__ == '__main__':
