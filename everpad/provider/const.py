f448d1414671d629ce2b27948dddbf4cb01f60fd
mgreene <mikeos2@gmail.com>
2014-06-10 06:20:26 -0400

    Change key - looking at problem: A full sync is completed.  A note is added from the web interface.  GN sees the need for an incremental update, but the database is corrupted.  Note, keeps running for additional notes.

--- everpad/provider/const.py
+++ everpad/provider/const.py
@@ -1,14 +1,8 @@
 # Constants 
 
-# using everpad constants for test
-CONSUMER_KEY = 'nvbn-1422'
-CONSUMER_SECRET = 'c17c0979d0054310'
-APP_URL = "http://everpad/"
-
-
-#CONSUMER_KEY = 'mgreene-8953'
-#CONSUMER_SECRET = '1bd7dc2f8c2eca83'
-#APP_URL = "http://gevernote/"
+CONSUMER_KEY = 'mgreene-8953'
+CONSUMER_SECRET = '1bd7dc2f8c2eca83'
+APP_URL = "http://gevernote/"
 
 #HOST = 'sandbox.evernote.com/'
 #SANDBOX_ENABLE = True
